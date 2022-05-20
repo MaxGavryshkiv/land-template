f7600fba2ad1f85bdd2484a82306f1360441ee3e
Max Gavryshkiv <maks20355@gmail.com>
2022-05-20 13:22:21 +0300

    add coments

--- homework-9/src/js/01-color-switcher.js
+++ homework-9/src/js/01-color-switcher.js
@@ -1,13 +1,17 @@
+//const
 const startBttnRef = document.querySelector('[data-start]');
 const stopBttnRef = document.querySelector('[data-stop]');
 const body = document.body;
 let intervalId;
 
+//addEventListener
 startBttnRef.addEventListener('click', startSwichColor);
 stopBttnRef.addEventListener('click', stopSwichColor);
 
+//default functionaly
 stopBttnRef.disabled = true;
 
+//functions
 function startSwichColor() {
   startBttnRef.disabled = true;
   stopBttnRef.disabled = false;
